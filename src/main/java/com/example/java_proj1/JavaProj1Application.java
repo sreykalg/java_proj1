63dfcae7d80083a8ad160be940fb023fff6051b1
sreykalg <lengsreyka.apple@gmail.com>
2025-08-07 09:51:27 +0700

    first commit

---
+++ src/main/java/com/example/java_proj1/JavaProj1Application.java
@@ -0,0 +1,13 @@
+package com.example.java_proj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProj1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_proj1/JavaProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_proj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
