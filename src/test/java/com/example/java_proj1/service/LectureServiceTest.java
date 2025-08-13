1ab6fafbbc4d9fc2c0c62ffb4932727f04e4448d
sreykalg <lengsreyka.apple@gmail.com>
2025-08-13 20:47:38 +0700

    [Update] Final Test

--- src/test/java/com/example/java_proj1/service/LectureServiceTest.java
+++ src/test/java/com/example/java_proj1/service/LectureServiceTest.java
@@ -23,28 +23,32 @@ class LectureServiceTest {
 
     @Test
     void registerAndListLectures() {
+        // given
         LectureDTO dto = LectureDTO.builder()
                 .title("Java + Spring Boot")
                 .content("Basics of AI")
                 .createdDate(LocalDate.now())
                 .build();
 
+        // when
         LectureDTO savedDto = lectureService.register(dto);
         List<LectureDTO> lectures = lectureService.findAll();
 
+        // then
         Assertions.assertThat(lectures).extracting("title").contains("Java + Spring Boot");
         Assertions.assertThat(savedDto.getLectureId()).isNotNull();
     }
 
     @Test
     void updateLecture() {
-
+        // given
         LectureDTO dto = LectureDTO.builder()
                 .title("Old Title")
                 .content("Old Content")
                 .createdDate(LocalDate.now())
                 .build();
 
+        // when
         LectureDTO savedDto = lectureService.register(dto);
 
         LectureDTO updateDto = LectureDTO.builder()
@@ -54,6 +58,7 @@ class LectureServiceTest {
 
         LectureDTO updatedDto = lectureService.update(savedDto.getLectureId(), updateDto);
 
+        // then
         Assertions.assertThat(updatedDto.getTitle()).isEqualTo("New Title");
         Assertions.assertThat(updatedDto.getContent()).isEqualTo("Updated Content");
     }
